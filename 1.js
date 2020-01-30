77dbd7d5023f422b77f961d96b566296d0fcedd4
= <=>
2020-01-30 20:55:10 +0700

    co ban xong file 1.js

---
+++ 1.js
@@ -0,0 +1,22 @@
+﻿ $(function(){
+ 	//xu ly cho nut .nut click vao
+ 	$(".nut").click(function(event) {
+ 		
+ 		//xu ly class cho noi dung
+ 		$(".noidung").addClass('noidunghienra');
+
+ 		//xu ly class cho .momo
+ 		$(".momo").addClass('momodira');
+ 	});
+
+ 	//Jquery cho nut close
+ 	$(".nutdong, .momo").click(function(event) {
+ 		
+ 		//xu ly class cho noi dung
+ 		$(".noidung").removeClass('noidunghienra');
+
+ 		//xu ly class cho .momo
+ 		$(".momo").removeClass('momodira');
+ 	});
+})  
+ 
